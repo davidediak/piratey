711a6d1b785aeea538fc4bf521099057ebbfe9a2
cmgriffing <cmgriffing@users.noreply.github.com>
2020-02-17 19:05:02 +0000

    deployed via Scully Publish Action 🎩 for c9734de1eef7082a03a921dc6336bcd09303f8d8

---
+++ home-home-module-es5.js
@@ -0,0 +1,71 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["home-home-module"], {
+  /***/
+  "./src/app/home/home.module.ts":
+  /*!*************************************!*\
+    !*** ./src/app/home/home.module.ts ***!
+    \*************************************/
+
+  /*! exports provided: HomeModule */
+
+  /***/
+  function srcAppHomeHomeModuleTs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    __webpack_require__.r(__webpack_exports__);
+    /* harmony export (binding) */
+
+
+    __webpack_require__.d(__webpack_exports__, "HomeModule", function () {
+      return HomeModule;
+    });
+    /* harmony import */
+
+
+    var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+    /*! @angular/core */
+    "./node_modules/@angular/core/__ivy_ngcc__/fesm2015/core.js");
+    /* harmony import */
+
+
+    var _angular_common__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+    /*! @angular/common */
+    "./node_modules/@angular/common/__ivy_ngcc__/fesm2015/common.js");
+
+    var HomeModule = function HomeModule() {
+      _classCallCheck(this, HomeModule);
+    };
+
+    HomeModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineNgModule"]({
+      type: HomeModule
+    });
+    HomeModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjector"]({
+      factory: function HomeModule_Factory(t) {
+        return new (t || HomeModule)();
+      },
+      imports: [[_angular_common__WEBPACK_IMPORTED_MODULE_1__["CommonModule"]]]
+    });
+
+    (function () {
+      (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵsetNgModuleScope"](HomeModule, {
+        imports: [_angular_common__WEBPACK_IMPORTED_MODULE_1__["CommonModule"]]
+      });
+    })();
+    /*@__PURE__*/
+
+
+    (function () {
+      _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](HomeModule, [{
+        type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"],
+        args: [{
+          declarations: [],
+          imports: [_angular_common__WEBPACK_IMPORTED_MODULE_1__["CommonModule"]]
+        }]
+      }], null, null);
+    })();
+    /***/
+
+  }
+}]);
+//# sourceMappingURL=home-home-module-es5.js.map
